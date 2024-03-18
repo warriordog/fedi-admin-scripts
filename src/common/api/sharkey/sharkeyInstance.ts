8fbf38571f727a06efd8b9bfd6c194b29b33cb5b
Hazel Koehler <acomputerdog@gmail.com>
2024-03-18 12:25:37 -0400

    fill out sharkey API entities

--- src/common/api/sharkey/sharkeyInstance.ts
+++ src/common/api/sharkey/sharkeyInstance.ts
@@ -1,8 +1,27 @@
 export interface SharkeyInstance {
+    id: string;
+    firstRetrievedAt: string;
     host: string;
+    usersCount: number;
+    notesCount: number;
+    followingCount: number;
+    followersCount: number;
+    isNotResponding: number;
     isSuspended: boolean;
     isBlocked: boolean;
+    softwareName: string | null;
+    softwareVersion: string | null;
+    openRegistrations: boolean;
+    name: string | null;
+    description: string | null;
+    maintainerName: string | null;
+    maintainerEmail: string | null;
     isSilenced: boolean;
+    iconUrl: string | null;
+    faviconUrl: string | null;
+    themeColor: string | null;
+    infoUpdatedAt: string;
+    latestRequestReceivedAt: string | null;
     isNSFW: boolean;
     moderationNote: string;
 }
